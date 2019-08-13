9ff21703643397211f8f08fb2cc18aa885dc8cd4
Kaique Magno <kaique.magno@hotmail.com>
2019-08-13 14:09:53 -0300

    Initial Commit

---
+++ SwiftUI-AsyncAPI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-AsyncAPI
+//
+//  Created by Kaique Magno Dos Santos on 13/08/19.
+//  Copyright © 2019 Kaique Magno Dos Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ SwiftUI-AsyncAPITests/SwiftUI_AsyncAPITests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_AsyncAPITests.swift
+//  SwiftUI-AsyncAPITests
+//
+//  Created by Kaique Magno Dos Santos on 13/08/19.
+//  Copyright © 2019 Kaique Magno Dos Santos. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_AsyncAPI
+
+class SwiftUI_AsyncAPITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
